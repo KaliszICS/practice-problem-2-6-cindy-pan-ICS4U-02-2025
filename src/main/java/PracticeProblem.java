3c607a36a435cda83b1e60fecc584971e523d1d5
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-04-03 13:56:45 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,49 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+	
+	public static int[] bubbleSort(double[] nums){
+		int[] h= {0,0};
+		int countcomp = 0;
+		int countswap = 0;
+		boolean sorted = true;
+		for (int j = 0; j<nums.length-1; j++){
+			sorted = false;
+			for (int i = 0; i < nums.length -1 - j; i++){
+				countcomp++;
+				if (nums[i]>nums[i+1]){
+					countswap = countswap +3;
+					double temp = nums[i];
+					nums[i] = nums [i+1];
+					nums [i+1] = temp;
+					sorted = true;
+				}
+			}
+		if (!sorted){
+			j = nums.length+1;
+			h[1] = countcomp;
+			h[0] = countswap;
+		}
+		}
+		
+		return h;
 	}
+	public static int[] selectionSort(double[] nums){
+		int[] help={0,0};
+		int countcomp = 0;
+		int countswap = 0;
 
-	public static void q1() {
-		//Write question 1 code here
+		for (int i= 1; i<nums.length; i++){
+			int min = i;
+			for (int j = i+1; j<nums.length; j++){
+				countcomp++;
+				if()
+			}
+		}
+
+		return help;
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static int[] insertionSort(double[] nums){
+		int help[]= {0,0};
+		return help;
 	}
 
 }
