19fae97a824161c21cc8bf2cd5dfd4a2b032232a
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-04-07 00:17:04 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,8 @@
 public class PracticeProblem {
+
+	public static void main(String args[]) {
+		leastSwaps(new double[]{9, 7, 4, 2, 1, 0});
+	}
 	
 	public static int[] bubbleSort(double[] nums){
 		int[] h= {0,0};
@@ -19,44 +23,121 @@ public class PracticeProblem {
 			}
 		if (!sorted){
 			j = nums.length+1;
-			h[1] = countcomp;
+			
+		}
+		}
+		h[1] = countcomp;
 			h[0] = countswap;
-		}
-		}
-		
 		return h;
 	}
 
-	
+
 	public static int[] selectionSort(double[] nums){
 		int[] help={0,0};
 		int countcomp = 0;
 		int countswap = 0;
 
-		for (int i= 1; i<nums.length; i++){
-			int min = i;
+		for (int i= 0; i<nums.length; i++){
+			int min = i; //swap 1
+			countswap++;
 			for (int j = i+1; j<nums.length; j++){
 				countcomp++;
 				if(nums[j]<nums[min]){
 					min = j;
-			
+				
 				}
 			}
-			if (min!=i){
-				double temp = nums[i];
-				nums[i]=nums[min];
-				nums[min]=temp;
-				countswap++;
-				help[0] = countswap;
-			}
-			help[1]=countcomp;
+				double temp = nums[i]; 
+				nums[i]=nums[min]; //swap 3
+				nums[min]=temp; //swap 4
+				countswap=countswap+3;	
 		}
-
+		help[1]=countcomp;
+		help[0] = countswap;
 		return help;
 	}
 	public static int[] insertionSort(double[] nums){
 		int help[]= {0,0};
+		int countcomp = 0;
+		int countswap = 0;
+
+		for (int i = 1; i < nums.length; i++) {
+			countswap++;
+			double key = nums[i];
+			int index = i - 1;
+			while (index >= 0 && nums[index] > key) { //check for -1 first
+				nums[index + 1] = nums[index];
+				index--;
+				countcomp++;
+				countswap++;
+			}
+			nums[index + 1] = key;
+			countswap++;
+		}
+		help[1]=countcomp;
+		help[0] = countswap;
 		return help;
 	}
 
+
+	public static String leastIterations(double[] nums){
+		int len = nums.length;
+		double[] bubble = new double[len];
+		double[] sele = new double [len];
+		double[] insert = new double [len];
+		for (int a =0; a<len; a++){
+			bubble[a] = nums[a];
+			sele[a] = nums[a];
+			insert[a] = nums[a];
+		}
+
+
+		int countbubble = bubbleSort(bubble)[1];
+		int countcompSelection = selectionSort(sele)[1];
+		int countInsert = insertionSort(insert)[1];
+		
+		if (countbubble <= countInsert && countbubble <= countcompSelection){
+			return "Bubble";
+		}
+		else if (countInsert <= countcompSelection){
+			return "Insertion";
+		}
+		else {
+			return "Selection";
+		}
+	}
+
+
+
+	public static String leastSwaps(double[] nums){
+		int len = nums.length;
+		double[] bubble = new double[len];
+		double[] sele = new double [len];
+		double[] insert = new double [len];
+		for (int a =0; a<len; a++){
+			bubble[a] = nums[a];
+			sele[a] = nums[a];
+			insert[a] = nums[a];
+		}
+
+
+		int countbubble = bubbleSort(bubble)[0];
+		int countcompSelection = selectionSort(sele)[0];
+		int countInsert = insertionSort(insert)[0];
+		System.out.println(countInsert);
+		System.out.println(countbubble);
+		System.out.println(countcompSelection);
+		
+		if (countbubble <= countInsert && countbubble <= countcompSelection){
+			return "Bubble";
+		}
+		else if (countInsert <= countcompSelection){
+			return "Insertion";
+		}
+		else {
+			return "Selection";
+		}
+		
+
+	}
 }
